3a7237576020ef532a7927afaa6d6e95fda3eefe
marcusadigun <adigunmarcus@gmail.com>
2026-04-13 08:04:15 -0400

    Updated config and fixed test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -2,19 +2,19 @@
 
 import { test, expect } from '@playwright/test';
 
-test('Browser Context Playwright test', async ({browser}) =>
+test.only('Browser Context Playwright test', async ({browser}) =>
 {
     //chrome - plugins/ cookies
        const context = await browser.newContext();
        const page = await context.newPage();
-       await page.goto("https://www.gidispot.com/login");
+       await page.goto('https://www.gidispots.com');
 
 });
 
 
 test('Page Playwright test', async ({page }) =>
 {
-    await page.goto('https://www.gidispot.com/login');
+    await page.goto("https://www.google.com");
     //
 
 
