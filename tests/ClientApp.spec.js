027f28f40398911a90f2e8f7706110dbebc2eef0
marcusadigun <adigunmarcus@gmail.com>
2026-04-16 09:44:36 -0400

    test: unmark error login test in `ClientApp.spec.js` as focused

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,7 +3,7 @@
 import { test, expect } from '@playwright/test';
 import expectedRestaurants from '../test-data/islandRestaurants.json' with { type: 'json' };
 
-test.only('Browser Context-Validating Error login', async ({page}) =>
+test('Browser Context-Validating Error login', async ({page}) =>
 {
   await page.goto('https://rahulshettyacademy.com/client')
   await page.getByRole('textbox', { name: 'email@example.com' }).fill('adigunmarcus@gmail.com');
